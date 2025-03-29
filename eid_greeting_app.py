5120a24db367fe8234f60a6ddb04bf2b9996e5e7
Abdurrahman Alowain <47498835+A-Owain@users.noreply.github.com>
2025-03-29 19:56:59 +0300

    Update eid_greeting_app.py

--- eid_greeting_app.py
+++ eid_greeting_app.py
@@ -37,7 +37,7 @@ if name:
     name_bbox = font.getbbox(bidi_name)
     name_width = name_bbox[2] - name_bbox[0]
     x_name = (image_width - name_width) / 2
-    y_name = 900
+    y_name = 1200
     # y_name = 4300
 
     # Draw name without shadow
