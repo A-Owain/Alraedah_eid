69b066d24f65115dfbcf10f4e506cea26bd5ab7d
Abdurrahman Alowain <47498835+A-Owain@users.noreply.github.com>
2025-03-29 19:52:08 +0300

    Update eid_greeting_app.py

--- eid_greeting_app.py
+++ eid_greeting_app.py
@@ -7,7 +7,7 @@ import os
 
 # Constants
 IMAGE_PATH = "eid-fitr.png"
-FONT_PATH = "IBMPlexSansArabic-Bold.ttf"  # Ensure this font file is in the repo root
+FONT_PATH = "Greta-Arabic-AR-LT-Bold.ttf"  # Ensure this font file is in the repo root
 
 st.set_page_config(page_title="Eid Greeting Generator", layout="centered")
 st.title("Eid Greeting Generator")
